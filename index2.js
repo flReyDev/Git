6a7f478b38a47deaeb2957578647317034bc14cd
Leonardo Rey <flrey@ucompensar.edu.co>
2023-02-25 16:12:07 -0500

    InitialCommit

---
+++ index2.js
@@ -0,0 +1,21 @@
+console.log("Curso Git y GitHub 2");
+//metodo que me ayude a validar un email valido
+//metodo que me ayude a validar un email valido
+
+
+// Path: index3.js
+// Compare this snippet from index.js:  
+
+function sumar(a, b) {
+    return a + b;
+}
+
+//COmo realizar metodo que se conecte por sokets a un servidor
+//how to connect to a server using sockets
+
+function onInit() {
+    console.log("Init app");
+}
+
+
+
